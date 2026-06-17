0108bd59fe4d1cc15436bf2567ce03c8d327d464
Firmanann <firmanardiyan02@gmail.com>
2026-06-17 14:42:22 +0700

    create starter project

---
+++ src/main/java/com/demisnack/Eduplay/Application/EduplayApplication.java
@@ -0,0 +1,13 @@
+package com.demisnack.Eduplay.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduplayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demisnack/Eduplay/Application/EduplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demisnack.Eduplay.Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
