1e29e010a0db3a372dd5780c1aa54341e52e61cd
Firmanann <firmanardiyan02@gmail.com>
2026-06-21 06:25:46 +0700

    fix cors config

--- src/main/java/com/demisnack/Eduplay/Application/security/config/CorsConfig.java
+++ src/main/java/com/demisnack/Eduplay/Application/security/config/CorsConfig.java
@@ -1,17 +1,24 @@
 package com.demisnack.Eduplay.Application.security.config;
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.addAllowedOriginPattern("*"); //origin
+        config.addAllowedHeader("*");        //header
+        config.addAllowedMethod("*");        //method
+        config.setAllowCredentials(true);    //token/cookies =true
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config); //all endpoint
+        return new CorsFilter(source);
     }
 }
